7f97e75ae1bfe2dd2a10ad75a67e211cc1c92928
Pankaj Prakash <pankaj.prakash@vasudhaika.net>
2020-04-12 17:15:28 +0530

    Moved logger functions to class

--- api/lib/logger.ts
+++ api/lib/logger.ts
@@ -20,54 +20,58 @@ function getDateLog(type: 'INFO' | 'WARN' | 'ERROR' | 'DEBUG' = 'INFO'): string 
   return env.production ? msgProd[type] : msgDev[type];
 }
 
-/**
- * Prints to `stdout` with newline.
- */
-export function log(message?: any, ...optionalParams: any[]): void {
+export class Logger {
 
-  if (typeof message === 'string') {
-    console.log(getDateLog('INFO') + message);
-  } else {
-    console.log(getDateLog('INFO'));
-    console.log(message);
+  /**
+   * Prints to `stdout` with newline.
+   */
+  static log(message?: any, ...optionalParams: any[]): void {
+
+    if (typeof message === 'string') {
+      console.log(getDateLog('INFO') + message);
+    } else {
+      console.log(getDateLog('INFO'));
+      console.log(message);
+    }
+
+    if (optionalParams.length > 0) {
+      console.log(optionalParams);
+    }
+
   }
 
-  if (optionalParams.length > 0) {
-    console.log(optionalParams);
+  /**
+   * Prints to `stderr` with newline.
+   */
+  static error(message?: any, ...optionalParams: any[]): void {
+
+    if (typeof message === 'string') {
+      console.error(getDateLog('ERROR') + message);
+    } else {
+      console.error(getDateLog('ERROR'));
+      console.error(message);
+    }
+
+    if (optionalParams.length > 0) {
+      console.error(optionalParams);
+    }
   }
 
+  /**
+   * The {@link console.warn()} function is an alias for {@link console.error()}.
+   */
+  static warn(message?: any, ...optionalParams: any[]): void {
+
+    if (typeof message === 'string') {
+      console.warn(getDateLog('WARN') + message);
+    } else {
+      console.warn(getDateLog('WARN'));
+      console.warn(message);
+    }
+
+    if (optionalParams.length > 0) {
+      console.warn(optionalParams);
+    }
+  }
 }
 
-/**
- * Prints to `stderr` with newline.
- */
-export function error(message?: any, ...optionalParams: any[]): void {
-
-  if (typeof message === 'string') {
-    console.error(getDateLog('ERROR') + message);
-  } else {
-    console.error(getDateLog('ERROR'));
-    console.error(message);
-  }
-
-  if (optionalParams.length > 0) {
-    console.error(optionalParams);
-  }
-}
-
-/**
- * The {@link console.warn()} function is an alias for {@link console.error()}.
- */
-export function warn(message?: any, ...optionalParams: any[]): void {
-
-  if (typeof message === 'string') {
-    console.warn(getDateLog('WARN') + message);
-  } else {
-    console.warn(getDateLog('WARN'));
-    console.warn(message);
-  }
-
-  if (optionalParams.length > 0) {
-    console.warn(optionalParams);
-  }
-}
