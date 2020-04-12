e55e1eee87933166428275cca0e4315d8809c26f
Pankaj Prakash <pankaj.prakash@vasudhaika.net>
2020-04-12 17:08:58 +0530

    Update environment files from js => ts

--- api/env/index.ts
+++ api/env/index.ts
@@ -2,7 +2,4 @@ import { environment as dev } from './dev.env';
 import { environment as prod } from './prod.env';
 
 // Define environment based on the command line env variable
-const env = process.env.NODE_ENV === 'production' ? prod : dev;
-
-// Environment export
-export default env;
+export const env = process.env.NODE_ENV === 'production' ? prod : dev;
